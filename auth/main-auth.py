da13b14b775d9df7a9282deaa111ce2d2795b564
JdcStig <duffycreagh2003@gmail.com>
2025-12-18 17:08:07 +0000

    Add Minio image storing and displaying public images

--- auth/main-auth.py
+++ auth/main-auth.py
@@ -92,6 +92,9 @@ class UserPublic(BaseModel):
     age: Optional[int] = None
     total_guesses: Optional[int] = 0
     total_correct: Optional[int] = 0
+    acc_guessing_ai: Optional[int] = 0
+    acc_guessing_real: Optional[int] = 0
+    
 
 
 class SignupRequest(BaseModel):
@@ -179,6 +182,8 @@ def build_user_public(doc: dict) -> UserPublic:
         age=doc.get("age"),
         total_guesses=doc.get("total_guesses", 0),
         total_correct=doc.get("total_correct", 0),
+        acc_guessing_ai=doc.get("acc_guessing_ai", 0),
+        acc_guessing_real=doc.get("acc_guessing_real", 0),
     )
 
 
@@ -288,6 +293,8 @@ async def signup(payload: SignupRequest):
         "age": None,
         "total_guesses": 0,
         "total_correct": 0,
+        "acc_guessing_ai": 0,
+        "acc_guessing_real": 0,
     }
 
     await users_coll.insert_one(user_doc)
