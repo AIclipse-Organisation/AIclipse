ebb062d69ddd5a37b720f94bc47c503e6d3be42f
NoahKrobot <noah@certus-sustavi.hr>
2026-01-15 15:40:01 +0100

    Add fixes

--- community/next.config.js
+++ community/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   basePath: "/community",
+  logging: {
+    incomingRequests: {
+      ignore: [/^\/healthz$/, /^\/api\/healthz$/],
+    },
+  }
 };
