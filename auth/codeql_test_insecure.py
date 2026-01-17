f70df11f2990c4252fbbfb79b846256947a4dca5
Roman Manzhelii <145562170+Roman-Manzhelii@users.noreply.github.com>
2026-01-17 18:13:25 +0000

    delete test files

--- auth/codeql_test_insecure.py
+++
@@ -1,8 +0,0 @@
-# codeql_test_insecure.py
-import pickle
-from flask import request
-
-def insecure_deserialize():
-    # INTENTIONALLY INSECURE (for CodeQL test): unsafe deserialization
-    data = request.get_data()
-    return pickle.loads(data)
