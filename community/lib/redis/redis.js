ee348945549b842264f8f689a3fcb50dc2719087
Sean Mooney <seanmooney1996@gmail.com>
2026-02-08 21:12:27 +0000

    fix: force add missing lib files for docker build

---
+++ community/lib/redis/redis.js
@@ -0,0 +1,38 @@
+import Redis from "ioredis";
+
+const host = process.env.REDIS_HOST || "redis-srv";
+const port = Number(process.env.REDIS_PORT || "6379");
+const password = process.env.REDIS_PASSWORD;
+
+const getRedisClient = () => {
+  const client = new Redis({
+    host,
+    port,
+    password,
+    maxRetriesPerRequest: 5,
+    enableReadyCheck: true,
+  });
+
+  client.on("error", (err) => {
+    console.error("[redis] error:", err?.message || err);
+  });
+
+  return client;
+};
+
+// Use global variable to preserve connection across module redoplyoments in dev . speeds up rebuilding
+// in development mode.
+let redis;
+
+if (process.env.APP_ENV === "dev") {
+  redis = getRedisClient();
+} else {
+  if (!global.redis) {
+    global.redis = getRedisClient();
+  }
+  redis = global.redis;
+}
+
+export function getRedis() {
+  return redis;
+}
