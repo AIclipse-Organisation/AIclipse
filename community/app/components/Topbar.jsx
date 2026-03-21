c20981408a568691b16376e3cfa05e2c824eb2c2
Roman Manzhelii <145562170+Roman-Manzhelii@users.noreply.github.com>
2026-03-21 15:35:23 +0000

    add main unit tests for client bff

--- community/app/components/Topbar.jsx
+++ community/app/components/Topbar.jsx
@@ -94,7 +94,7 @@ export default function Topbar({ isAdmin }) {
                 strokeLinecap="round"
                 strokeLinejoin="round"
               >
-                <path d="M19 21v-2a4 2 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
                 <circle cx="12" cy="7" r="4"></circle>
               </svg>
             </span>
